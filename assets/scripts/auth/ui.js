58d93ba1feffeb1f06a5a4669db84ceb9726669d
rakshyakc <rakshya.khatri1@gmail.com>
2018-11-22 12:30:48 -0500

    Features
    Created html document for contents.
    Added background video and sign up, sign in buttons.
    Created modal style for forms.
    Added auth folder with api, events and ui files to handle authorization.
    Added event handlers on app.js for sign up and sign in.

---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,72 @@
+'user strict'
+const store = require('../store.js')
+
+const resetSignUp = () => {
+  const x = document.getElementById('sign-up')
+  x.reset()
+}
+
+const resetSignIn = () => {
+  const x = document.getElementById('sign-in')
+  x.reset()
+}
+
+const resetChangePassword = () => {
+  const x = document.getElementById('change-password')
+  x.reset()
+}
+
+const signUpSuccess = data => {
+  // needs to redirect to sign in
+  $('#unAuthedMessage').html(`Sign up was successful. Sign in to start playing!`)
+  resetSignUp()
+}
+
+const signUpFailure = () => {
+  $('#unAuthedMessage').html(`Sorry, sign up was unsuccessful. Try again.`)
+  // console.log('signUpFailure ran. Error is :', error)
+  resetSignUp()
+}
+
+const signInSuccess = data => {
+  store.user = data.user
+  // console.log('signInSuccess ran. Data is :', data)
+  $('#unAuthedMessage').html('')
+  resetSignIn()
+  $('#all-games-IDs').html(` `)
+}
+const signInFailure = () => {
+  $('#unAuthedMessage').html(`Sorry, sign in was unsuccessful. Try again.`)
+  // console.log('signInFailure ran. Error is :', error)
+  resetSignIn()
+}
+
+const changePasswordSuccess = data => {
+  // console.log('changePasswordSuccess ran. Data is :', data)
+  $('#authedMessage').html(`Congratulations! Password was changed successfully!`)
+  resetChangePassword()
+}
+const changePasswordFailure = () => {
+  // console.log('changePasswordFailure ran. Error is :', error)
+  $('#authedMessage').html(`Sorry password couldn't be changed. Try again!`)
+  resetChangePassword()
+}
+
+const signOutSuccess = () => {
+  console.log('signOutSuccess ran.')
+  store.user.token = []
+}
+const signOutFailure = (error) => {
+  $('#authedMessage').html(`Sorry, sign out was unsuccessful. Try again.`)
+  console.log('signOutFailure ran. Error is :', error)
+}
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  changePasswordSuccess,
+  changePasswordFailure,
+  signOutSuccess,
+  signOutFailure
+}
