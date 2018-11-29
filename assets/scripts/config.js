bf5d468ce0fa12d631a000a004241e3fe9f363d5
rakshyakc <rakshya.khatri1@gmail.com>
2018-11-29 09:44:59 -0500

    Removed / from config url production

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://boiling-fjord-48869.herokuapp.com/',
+  production: 'https://boiling-fjord-48869.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
